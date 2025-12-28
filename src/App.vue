eef4d3c54b538ef37050ef15ef3f8c2949286822
390a <390and@gmail.com>
2025-12-28 14:44:45 +1000

    delete margin top

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
   background-color: rgb(3, 109, 29);
   min-height: 100vh;
 }
